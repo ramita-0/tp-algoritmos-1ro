95cba2c36433bec9a4922e5125a664bcdb27d973
ramita-0 <ramiromoya321@gmail.com>
2023-07-18 13:20:23 -0300

    Defined structs for Repartidor and Vehiculo entities

---
+++ shared/model/repartidor.model.h
@@ -0,0 +1,10 @@
+#include <iostream>;
+#include "./vehiculo.model.h";
+using namespace std;
+
+struct Repartidor {
+  string nombre;
+  string apellido;
+  int dni;
+  Vehiculo vehiculo;
+};
---
+++ shared/model/vehiculo.model.h
@@ -0,0 +1,8 @@
+#include <iostream>;
+#include "../enum/vehiculos.enum.h";
+using namespace std;
+
+struct Vehiculo {
+  Vehiculos tipo;
+  string patente;
+};
