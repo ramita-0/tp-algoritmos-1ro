b78dfb2c7d6683e7c0f1994c3ab0d3af2f5ff7c0
Benicio Gonzalez <benigc612@hotmail.com>
2023-07-27 23:15:37 -0300

    correcciones a informe de repartidores

--- modules/reports-actions.cpp
+++ modules/reports-actions.cpp
@@ -5,19 +5,18 @@
 using namespace std;
 
 void repartidoresPorZona(int matriz[][14], int size, int vectorSuma[]);
-void maximoZona(int vectorSuma[], int zonas[], int &j);
-void informeDeZonas(int vectorSuma[], int zonas[], int tamZonas);
+int maximoZona(int vectorSuma[]);
+void informeDeZonas(int vectorSuma[], int maximo);
 
 void Informe3 (Repartidor repartidores[], int cantidadRepartidoresActuales)
 {
-    int vectorSuma[14];
-    int zonas[14];
-    int tamZonas;
-    int M[5][14];
+    int vectorSuma[14]={0};
+    int M[5][14]={0};
+    int maximo;
     generarSumatoriaVehiculosZona(M,repartidores,cantidadRepartidoresActuales);
     repartidoresPorZona(M,cantidadRepartidoresActuales,vectorSuma);
-    maximoZona(vectorSuma,zonas,tamZonas);
-    informeDeZonas(vectorSuma,zonas,tamZonas);
+    maximo=maximoZona(vectorSuma);
+    informeDeZonas(vectorSuma,maximo);
 }
 
 void repartidoresPorZona(int matriz[][14], int size, int vectorSuma[])
@@ -32,36 +31,27 @@ void repartidoresPorZona(int matriz[][14], int size, int vectorSuma[])
     }
 }
 
-void maximoZona(int vectorSuma[], int zonas[], int &j)
+int maximoZona(int vectorSuma[])
 {
     int maximo;
-    for(int i=0;i<14;i++)
+    for(int i=0; i<14; i++)
     {
         if(i==0 || vectorSuma[i]>maximo)
         {
             maximo=vectorSuma[i];
-            j=0;
-            zonas[j]=i;
-            j++;
-        }
-        else
-        {
-            if(vectorSuma[i]==maximo)
-            {
-                zonas[j]=i;
-                j++;
-            }
         }
     }
+    return maximo;
 }
 
-void informeDeZonas(int vectorSuma[], int zonas[], int tamZonas)
+void informeDeZonas(int vectorSuma[], int m)
 {
-    int posicion;
     cout<<"Zona/s con mas repartidores (sin diferenciar el transporte):"<<endl;
-    for(int i=0; i<tamZonas; i++)
+    for(int i=0; i<14; i++)
     {
-        zonas[i]=posicion;
-        cout<<vectorSuma[posicion]-1<<endl;
+        if(vectorSuma[i]==m) cout<<i+1<<endl;
     }
+    cout<<"0 para volver"<<endl;
+    string variable;
+    cin>>variable;
 }
